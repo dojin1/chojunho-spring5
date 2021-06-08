469b5c1283ca5bfbb22ed59e3e55c82b10982d1d
ana27 <ana27@DESKTOP-PS56EJS>
2021-06-08 10:49:34 +0900

    0608 2교시 커밋

---
+++ src/main/java/com/edu/dao/IF_MemberDAO.java
@@ -0,0 +1,17 @@
+package com.edu.dao;
+
+import java.util.List;
+
+import com.edu.vo.MemberVO;
+
+/**
+ * 이 인터페이스는 회원관리에 관련된 CRUD 메서드 명세가 포함된파일.
+ * 이 인터페이스는 메서드명만 있고, {구현내용} 이 없는 목록파일.	
+ * @author 조준호
+ *
+ */
+public interface IF_MemberDAO {
+	// List<제네릭타입> : MemberVO 1개 레코드를 List클래스형 감싸주면
+	// 다수의 레코드를 저장할 수 있는 형태가 됩니다.
+	public List<MemberVO> selectMember() throws Exception;
+}
