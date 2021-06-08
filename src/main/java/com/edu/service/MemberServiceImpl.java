ae35edb097550021139eecc8d931053fcbd9c6f1
ana27 <ana27@DESKTOP-PS56EJS>
2021-06-08 14:17:14 +0900

    0608 5교시 중간커밋

---
+++ src/main/java/com/edu/service/MemberServiceImpl.java
@@ -0,0 +1,30 @@
+package com.edu.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.edu.dao.IF_MemberDAO;
+import com.edu.vo.MemberVO;
+
+/**
+ * 이 클래스는 회원관리 서비스의 인터페이스를 구현하는 클래스 입니다.
+ * 상속 extends, 구현implements 키워드를 사용.
+ * 스프릥빈으로 등록하려면, @Service 애너테이션을 명시.
+ * @author 조준호
+ *
+ */
+@Service
+public class MemberServiceImpl implements IF_MemberService                                 {
+	@Inject //IF_MemberDAO를 주입해서 객체로 사용(아래)
+	private IF_MemberDAO memberDAO;
+	@Override
+	public List<MemberVO> selectMember() throws Exception {
+		// 인터페이스에서 상속받은 메서드를 구현(아래)
+		List<MemberVO> listMember = memberDAO.selectMember();
+		return memberDAO.selectMember();
+	}
+
+}
