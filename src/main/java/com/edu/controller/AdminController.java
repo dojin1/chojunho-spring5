b5ad0a26380f59dc0eff0ffd9f585c855bd20a4e
chojunho <ana27@daum.net>
2021-06-22 11:43:16 +0900

    0622 tue 3class

--- src/main/java/com/edu/controller/AdminController.java
+++ src/main/java/com/edu/controller/AdminController.java
@@ -17,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.edu.service.IF_BoardService;
 import com.edu.service.IF_BoardTypeService;
 import com.edu.service.IF_MemberService;
+import com.edu.util.CommonUtil;
+import com.edu.vo.AttachVO;
 import com.edu.vo.BoardTypeVO;
+import com.edu.vo.BoardVO;
 import com.edu.vo.MemberVO;
 import com.edu.vo.PageVO;
 
@@ -41,12 +44,31 @@ public class AdminController {
 	private IF_BoardTypeService boardTypeService;
 	@Inject
 	private IF_BoardService boardService;//DI으로 스프링빈을 주입해서 객체로 생성
-	
+	@Inject
+	private CommonUtil commonUtil;
 	//게시물 상세보기 폼으로 접근하지 않고 URL쿼리 스트링으로 접근(Get)
 	@RequestMapping(value="/admin/board/board_view", method=RequestMethod.GET)
 	public String board_view(@RequestParam("bno")Integer bno,@ModelAttribute("pageVO")PageVO pageVO, Model model) throws Exception {
-		
-		model.addAttribute("boardVO", boardService.readBoard(bno));
+		BoardVO boardVO = boardService.readBoard(bno);
+		//첨부파일 부분 attach데이터도 board_view.jsp로 이동해야함(아래)
+		List<AttachVO> files = boardService.readAttach(bno);
+		//배열객체 생성구조: String[] 배열명 = new String[배열크기]
+		//개발자가 만든 클래스형 객체 boardVO는 개발자가 만든 메서드 사용
+		//반면, List<AttachVO> files List클래스형 객체 files는 내장형 메서드 = .size()
+		String[] save_file_names = new String[files.size()];
+		String[] real_file_names = new String[files.size()];
+		//attach테이블안의 해당 bno게시물의 첨부파일 이름을 파싱해서 jsp로 보내주는 과정(아래)
+		int cnt= 0;
+		for(AttachVO file_name:files) {//files에서 1개의 레코드씩 추출
+			save_file_names[cnt] = file_name.getSave_file_name();
+			real_file_names[cnt] = file_name.getReal_file_name();
+			cnt = cnt + 1;//= cnt++
+		}
+		boardVO.setSave_file_names(save_file_names);//파싱한 결과 Set//다운로드
+		boardVO.setReal_file_names(real_file_names);//boardVO에 Set//화면에 보이는데
+		model.addAttribute("boardVO", boardVO);
+		//업로드한 파일이 이미지인지 확인하는 용도의 데이터 입니다(목적: 이미지일때 미리보기 img태그를 사용하기 위해)
+		model.addAttribute("checkImgArray",commonUtil.getCheckImgArray());
 		return "admin/board/board_view";//jsp생략
 	}
 	//게시물 목록은 폼으로 접근하지 않고 URL로 접근하기 때문에 GET방식으로처리
